0626eee0de5615aeb87fedc040e5202aa84dad90
valeeb <valentin.leeb@tum.de>
2026-02-02 22:29:05 +0100

    corrected the conductivity calculations

--- src/alter_surf/utils_CuMnAs.py
+++ src/alter_surf/utils_CuMnAs.py
@@ -1,7 +1,11 @@
 import numpy as np
-from blochK.observable import conductivity_orbital_resolved
+
 from alter_surf.utils3D import project_doublelayer
 
+
+
+
+from blochK.observable import conductivity_list_of_operators
 
 def get_conductivity_layer_resolved(Hamiltonian, **conductivity_args):
     """Computes the layer resolved conductivity for a given DLKK Hamiltonian 
@@ -26,20 +30,21 @@ def get_conductivity_layer_resolved(Hamiltonian, **conductivity_args):
     assert hasattr(Hamiltonian, "operator") and getattr(Hamiltonian.operator, "spin", None) is not None, "Hamiltonian must have 'spin' operator defined."
     assert hasattr(Hamiltonian, "suboperator") and hasattr(Hamiltonian.suboperator, "proj"), "Hamiltonian must have 'proj' suboperator defined."
 
-    
-    # Calculate spin conductivity
-    cond_tensor = conductivity_orbital_resolved(Hamiltonian,**conductivity_args) #.shape=(layer, spin, sublattice, x, y)
-    
-    spin_cond = []; cond0 = []
-    layers = np.arange(0,Hamiltonian.param['len_z']-1,1) #specify 
+    # Constrcut the layers. 
+    layers = np.arange(0,Hamiltonian.param['len_z']-1,1)
 
+    #construct the list of projectors
+    operators = []
     for layer in layers:
-        Dens_op_layer = Hamiltonian.suboperator.proj.dot(project_doublelayer(layer,Hamiltonian))
-        Spin_op_layer = Hamiltonian.suboperator.proj.dot(project_doublelayer(layer,Hamiltonian))*Hamiltonian.operator.spin #projected spin_operator
-        spin_cond.append(np.sum(Spin_op_layer[:,None,None]*cond_tensor,axis=0))
-        cond0.append(np.sum(Dens_op_layer[:,None,None]*cond_tensor,axis=0))
+        operators.append([Hamiltonian.suboperator.proj.dot(project_doublelayer(layer,Hamiltonian)),
+                        Hamiltonian.suboperator.proj.dot(project_doublelayer(layer,Hamiltonian))*Hamiltonian.operator.spin])
+    operators = np.swapaxes(operators,0,1) #.shape=(2, n_layers,localH,localH)
+    
 
-    spin_cond = np.array(spin_cond)
-    cond0 = np.array(cond0)
+    cond_tensor = conductivity_list_of_operators(Hamiltonian,list_of_operators=operators,optimize=['einsum_path', (0, 6), (1, 5), (1, 5), (1, 4), (1, 2), (1, 2), (0, 1)],**conductivity_args)
+    cond = cond_tensor[0]
+    spin_cond = cond_tensor[1]
 
-    return layers, cond0, spin_cond
+    return layers, cond, spin_cond
+
+
--- src/alter_surf/utils_DLKK.py
+++ src/alter_surf/utils_DLKK.py
@@ -1,5 +1,6 @@
 import numpy as np
-from blochK.observable import conductivity_orbital_resolved
+from blochK.observable import conductivity_list_of_operators
+from alter_surf.utils3D import project_doublelayer
 
 
 def get_conductivity_layer_resolved(Hamiltonian, **conductivity_args):
@@ -13,29 +14,22 @@ def get_conductivity_layer_resolved(Hamiltonian, **conductivity_args):
     How to use the results to get the spin splitting angle (in degrees)
         2*np.arctan(spin_cond[:,0,0]/cond0[:,0,0])*180/pi
     """
-    len_z = Hamiltonian.param['len_z']
-    # Calculate spin conductivity
-    cond_tensor = conductivity_orbital_resolved(Hamiltonian,**conductivity_args) #.shape=(layer, spin, sublattice, x, y)
-    
-    #1) way of dealing with this tensor: smart reshaping and then contracting all unwanted indices
-    n_layers = 2 #sum always n_layers layers up
-    cond_tensor2 = np.reshape(cond_tensor,(len_z,2,2,2,2)) #.shape=(layer,spin,sublattice,x,y)
-    cond_tensor2 = cond_tensor2.reshape(-1,n_layers,2,2,2,2) #.shape=(layer/n_layers,n_layers,sublattice,spin,x,y)
-    cond = np.sum(cond_tensor2[:,:,:,:,:,:],axis=(1,3)) #.shape=(layer/n_layers,spin,i=(x,y),j=(x,y))
-    spin_cond = cond[:,0]-cond[:,1] #difference of spin indices
-    cond0 = cond[:,0]+cond[:,1] #total cond: sum of spin indices
     #indices is fixed by n_layers
+    len_z = Hamiltonian.param['len_z']
+    n_layers = 2 #sum always n_layers layers up
     layers = np.arange(0,len_z+1-n_layers,n_layers)
 
-    # #2) way of dealing with this tensor: construct full observable and then contract (slower but does't matter)
-    # #we can compute pairs (0,1), (1,2), (2,3), ...
-    # spin_cond00 = []; cond00 = []
-    # layers = np.arange(0,param['len_z']-1,1) #specify 
+    #construct the list of projectors
+    operators = []
+    for layer in layers:
+        operators.append([project_doublelayer(layer,Hamiltonian),
+                          project_doublelayer(layer,Hamiltonian)*Hamiltonian.operator.spin])
+    operators = np.swapaxes(operators,0,1) #.shape=(2, n_layers,localH,localH)
+    
+    cond_tensor = conductivity_list_of_operators(Hamiltonian,list_of_operators=operators,optimize=['einsum_path', (0, 6), (1, 5), (1, 5), (1, 4), (1, 2), (1, 2), (0, 1)],**conductivity_args)
+    cond = cond_tensor[0]
+    spin_cond = cond_tensor[1]
 
-    # for layer in layers:
-    #     proj_2layers = projector2layer(layer,len_z=param['len_z'])+projector2layer(layer+1,len_z=param['len_z'])
-    #     Spin_op_layer = np.kron(proj_2layers,Spin_operator) #projected spin_operator
-    #     spin_cond00.append(np.sum(Spin_op_layer*cond_tensor[:,0,0]))
-    #     cond00.append(np.sum(np.kron(proj_2layers,np.ones(4))*cond_tensor[:,0,0]))
+    return layers, cond, spin_cond
 
-    return layers, cond0, spin_cond
+
